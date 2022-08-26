7fc143375aeac0ff7a2346ddf84fb19f0656c7c8
504aguiluz <aguiluz.matt@gmail.com>
2022-08-26 17:15:49 -0500

    resctructure and gitignore

--- parallels/lib/audio signal/wave.ex
+++
@@ -1,7 +0,0 @@
-defmodule Wave do
-  @type t :: %__MODULE__{
-          frequency: integer(),
-          gain: integer()
-        }
-  defstruct [:frequency, :gain]
-end
--- parallels/test/parallels_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ParallelsTest do
-  use ExUnit.Case
-  doctest Parallels
-
-  test "greets the world" do
-    assert Parallels.hello() == :world
-  end
-end
